a80a2532278e365b41d6ca7fffaa7e0b7b6b04fb
Miloš Stojsavljević <skolsic993@gmail.com>
2023-04-10 13:48:08 +0200

    Add basic password validator in javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+function isValidPassword(password, username) {
+  if (password.length < 8) {
+    return false;
+  }
+
+  if (password.indexOf(" ") !== -1) {
+    return false;
+  }
+
+  if (password.indexOf(username) !== -1) {
+    return false;
+  }
+
+  return true;
+}
+
+const result = isValidPassword("somethingnew");
+
+console.log(result);
